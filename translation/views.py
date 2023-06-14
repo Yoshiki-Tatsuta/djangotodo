1ee85d64b1a3ce29bced93adefec70130db21956
龍田 佳樹 <yo-tatsuta@a.tgl.co.jp>
2023-06-14 11:09:33 +0900

    translate

--- translation/views.py
+++ translation/views.py
@@ -1,25 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-import openai
+from deep_translator import GoogleTranslator
 
 # Create your views here.
 
-openai.api_key = '***'
-
-
 def index(request): 
     if request.method == 'POST':
-        input_data = request.POST['chatai']
-        def ask_gpt(prompt):
-            res = openai.ChatCompletion.create(
-            model='gpt-3.5-turbo',
-            messages=[{'role': 'user', 'content': prompt}])
-            
-            return res.choices[0]['message']['content']
-        
-        chat_data = ask_gpt(input_data)
-        
-        return render(request, 'translation/index.html', {'chat_data': chat_data})
+        input_data = request.POST['trans']
+        trans_data = GoogleTranslator(source='auto', target='ja').translate(input_data)      
+        return render(request, 'translation/index.html', {'trans_data': trans_data})
     else:
         return render(request, "translation/index.html")
